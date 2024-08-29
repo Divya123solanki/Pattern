d6ffe599a68a1ca6c2a6c6e36900018880a1e140
Divya123solanki <solankidivya817@gmail.com>
2024-08-29 07:10:48 +0530

    Add files via upload

---
+++ Pattern_8.java
@@ -0,0 +1,10 @@
+public class Pattern_8 {
+    public static void main(String args[]){
+        for(int i=4;i>=1;i--){
+            for(int j=i;j<=4;j++){
+                System.out.print(j%2);
+            }
+            System.out.println();
+        }
+    }
+}
