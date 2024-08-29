cd992fb6f525a090cf8eb29a83879c6289b78869
Divya123solanki <solankidivya817@gmail.com>
2024-08-29 07:10:18 +0530

    Add files via upload

---
+++ Pattern_6.java
@@ -0,0 +1,20 @@
+/*12345
+  AAAAA
+  12345
+ */
+public class Pattern_6 {
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if(i%2==0){
+                    System.out.print("A");
+                }
+                else{
+                    System.out.print(j);
+                }
+            }
+                System.out.println();
+            }
+        }
+    }
+
