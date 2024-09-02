eebb1cd7cd88ec6229d0ee0574f3c7ecf707e29f
Divya123solanki <solankidivya817@gmail.com>
2024-09-02 13:20:26 +0530

    Add files via upload

---
+++ Pattern_13.java
@@ -0,0 +1,16 @@
+ /*0
+   10
+   010
+   1010
+  */
+ class Pattern_13 {
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+           for(int j=i;j<=4;j++){
+            System.out.print(j%2);
+           }
+        }
+           System.out.println();
+        }
+    }
+
