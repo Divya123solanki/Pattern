2ab71f18f92c424d56a1e62bb42ee6858c8b6c96
Divya123solanki <solankidivya817@gmail.com>
2024-09-08 10:51:58 +0530

    Add files via upload

---
+++ p7.java
@@ -0,0 +1,16 @@
+ class p7 {
+    public static void main(String args[]){
+        int s=5;
+        for(int i=9;i>=1;i--,i--){
+            for(int k=9;k>=i;k--){
+                System.out.print(" ");
+            }{
+            for(int j=1;j<=i;j++){
+                System.out.print(" "+s);
+            }
+            s--;
+        }
+        System.out.println();
+    }
+}
+ }
