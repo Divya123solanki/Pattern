84df1f958a1ac293b1099b375b005e105cbc2908
Divya123solanki <solankidivya817@gmail.com>
2024-09-11 09:45:26 +0530

    Add files via upload

---
+++ p15.java
@@ -0,0 +1,16 @@
+public class p15 {
+    public static void main(String args[]){
+        for(int i=1;i<=7;i++){
+            if(i%2==0){
+                System.out.print(" ");
+            }
+            else{
+
+            }
+            for(int j=1;j<=7;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
