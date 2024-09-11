ed13eb5e30eacdfaa61b43494c93923d1f288cb5
Divya123solanki <solankidivya817@gmail.com>
2024-09-11 09:45:45 +0530

    Add files via upload

---
+++ p16.java
@@ -0,0 +1,10 @@
+public class p16 {
+    public static void main(String args[]){
+        int a=1;
+        System.out.print(a);{
+            for(int i=1;i<=5;i++){
+                System.out.print(" "+i*i*i);
+            }
+        }
+    }
+}
