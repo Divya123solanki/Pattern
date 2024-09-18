39060749ab72a4fa1e9b4fbc1400243103c01dad
Divya123solanki <solankidivya817@gmail.com>
2024-09-18 10:02:11 +0530

    Add files via upload

---
+++ p29.java
@@ -0,0 +1,13 @@
+public class p29 {
+    public static void main(String args[]){
+        for(int i=1;i<=9;i++,i++){
+            for(int k=9;k>=i;k--){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
