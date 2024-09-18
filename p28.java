6cbdd1de89e2260d772199c5267333037618e093
Divya123solanki <solankidivya817@gmail.com>
2024-09-18 10:01:49 +0530

    Add files via upload

---
+++ p28.java
@@ -0,0 +1,14 @@
+public class p28 {
+    public static void main(String args[]){
+        char ch=97;
+        for(int i=5;i>=1;i--){
+            for(int k=5;k>=i;k--){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(" "+ch++);
+            }
+            System.out.println();
+        }
+    }
+}
