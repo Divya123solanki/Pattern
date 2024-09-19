4f8adbdaf209c623279f8b8095d3c250c260a588
Divya123solanki <solankidivya817@gmail.com>
2024-09-19 11:44:30 +0530

    Add files via upload

---
+++ p30.java
@@ -0,0 +1,15 @@
+public class p30 {
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+               if(j==1||i==1||i+j==6){
+                System.out.print("*");
+               }
+               else{
+                System.out.print(" ");
+               }
+            }
+            System.out.println();
+        }
+    }
+}
